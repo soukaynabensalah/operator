087281c8d8d34e65957e1e94a76ffc9eb5ba8acc
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-10-09 00:29:43 +0100

    Create Operator.py

---
+++ Operator.py
@@ -0,0 +1,24 @@
+a=int(input("entrer la valeur de a))
+b=int(input("entrer la valeur de b))
+op=str(input("entrer l'opérateur"))
+match op :
+  case "+" :
+    print(a,"+",b,"=",a+b)
+    case "-" :
+    print(a,"-",b,"=",a-b)
+    case "*" :
+    print(a,"*",b,"=",a*b)
+    case "/" :
+    if b=0 :
+     print("impossible")
+    else
+    print(a,"/",b,"=",a/b)
+    case "mod" :
+    print(a,"mode",b,"=",amodb)
+    case "+" :
+    print(a,"mode",b,"=",amodeb)
+    
+    
+    
+
+    
